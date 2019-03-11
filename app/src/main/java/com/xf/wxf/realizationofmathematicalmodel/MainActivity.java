daf87b888ce51a7efd606ae82839701764013ea5
dillon <dillon66@163.com>
2019-03-11 15:45:55 +0800

    涂鸦的学习

--- app/src/main/java/com/xf/wxf/realizationofmathematicalmodel/MainActivity.java
+++ app/src/main/java/com/xf/wxf/realizationofmathematicalmodel/MainActivity.java
@@ -3,9 +3,7 @@ package com.xf.wxf.realizationofmathematicalmodel;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.xf.wxf.realizationofmathematicalmodel.combination.Combination;
 
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
